48260a95474530a66c5f5f4120b619137424f822
David Amey <damey@worth.systems>
2020-03-28 21:23:50 +0000

    renaming namespace rooms for relevance

--- data/namespaces.js
+++ data/namespaces.js
@@ -3,14 +3,15 @@ const Namespace =  require('../classes/Namespace');
 const Room =  require('../classes/Room');
 
 // Set up the namespaces
+// TODO: this should come from a DB
 let namespaces = [];
 let worthNs = new Namespace(0,'Worth','https://images.ctfassets.net/bwx98rfv5w3w/3Xzy1AQAXmCm0iK62iAiOm/7bb9e0d9cd0020949d4554f4c66ed785/worth-logo-svg.svg','/worth');
 
 namespaces.push(worthNs);
 
 // Make the main room and add it to rooms. it will ALWAYS be 0
-worthNs.addRoom(new Room(0,'New Articles','Worth'));
-worthNs.addRoom(new Room(1,'Editors','Worth'));
-worthNs.addRoom(new Room(2,'Other','Worth'));
+worthNs.addRoom(new Room(0,'Red','Worth'));
+worthNs.addRoom(new Room(1,'General','Worth'));
+worthNs.addRoom(new Room(2,'Random','Worth'));
 
 module.exports = namespaces;
